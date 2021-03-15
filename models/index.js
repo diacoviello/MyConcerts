65a9a82a0cc8fa1d15a66792fe4a8a659dabc76c
Dave <diacovmusic@gmail.com>
2021-03-15 19:11:19 -0400

    looks great

---
+++ models/index.js
@@ -0,0 +1,27 @@
+const User = require("./User");
+
+const Concerts = require("./Concerts");
+const Location = require("./Location");
+
+
+Concerts.belongsToMany(Location, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: User,
+    unique: false,
+  },
+  // Define an alias for when data is retrieved
+  as: "stored_data",
+});
+
+Location.belongsToMany(Concerts, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: User,
+    unique: false,
+  },
+  // Define an alias for when data is retrieved
+  as: " ",
+});
+
+module.exports = { Concerts, Location, User };
